c97a549fa82e3f41a73aa62bf3c0748e329091cb
YICHAEGYEONG <48883739+chaechaeyi@users.noreply.github.com>
2023-05-18 10:51:45 +0900

    [level 3] Title: N으로 표현, Time: 3.59 ms, Memory: 71.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42895. N으로 표현/N으로 표현.java
@@ -0,0 +1,36 @@
+class Solution {
+    
+    private int n;
+    private int target;
+    private int answer = Integer.MAX_VALUE;
+
+    public int solution(int N, int number) {
+        n = N;
+        target = number;
+        dfs(0, 0);        
+        return answer == Integer.MAX_VALUE ? -1 : answer;
+    }
+
+    private void dfs(int count, int prev) {
+        if (count > 8) {
+            answer = -1;
+            return;
+        }
+
+        if (prev == target) {
+            answer = Math.min(answer, count);
+            return;
+        }
+
+        int tempN = n;
+        for (int i = 0; i < 8 - count; i++) {
+            int newCount = count + i + 1;
+            dfs(newCount, prev + tempN);
+            dfs(newCount, prev - tempN);
+            dfs(newCount, prev / tempN);
+            dfs(newCount, prev * tempN);
+
+            tempN = tempN * 10 + n;
+        }
+    }
+}

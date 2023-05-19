bebbc52dce91b815e4b164903b9e93d4f2c483f3
YICHAEGYEONG <48883739+chaechaeyi@users.noreply.github.com>
2023-05-19 22:38:23 +0900

    [level 1] Title: K번째수, Time: 0.30 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for(int i=0; i<commands.length; i++){
+            int[] temp = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+            Arrays.sort(temp);
+            answer[i] = temp[commands[i][2]-1];
+        }
+
+        return answer;
+    }
+}

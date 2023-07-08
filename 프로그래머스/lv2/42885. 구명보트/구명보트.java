cf09ecb4188d429a7aa739b6ca1dbc4a46c533ed
YICHAEGYEONG <48883739+chaechaeyi@users.noreply.github.com>
2023-07-08 19:42:31 +0900

    [level 2] Title: 구명보트, Time: 11.31 ms, Memory: 56.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.java
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.java
@@ -7,7 +7,8 @@ class Solution {
         int min = 0;
 
         for (int max = people.length - 1; min <= max; max--){
-          if (people[min] + people[max] <= limit) min++;
+          if (people[min] + people[max] <= limit) 
+            min++;
           answer++;
         }
 

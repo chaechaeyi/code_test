e7370edc4916f23054e97bd5228424f2f1c132c7
YICHAEGYEONG <48883739+chaechaeyi@users.noreply.github.com>
2024-12-09 23:27:40 +0900

    [Bronze V] Title: A+B, Time: 172 ms, Memory: 17764 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        	Scanner in = new Scanner(System.in);
+		int A = in.nextInt();
+		int B = in.nextInt();
+		
+		System.out.println(A+B);
+ 
+		in.close();
+    }
+}

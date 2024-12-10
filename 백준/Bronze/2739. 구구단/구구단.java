524dceb2555d49a95851e3ced928671c9b085fc8
YICHAEGYEONG <48883739+chaechaeyi@users.noreply.github.com>
2024-12-11 00:24:15 +0900

    [Bronze V] Title: 구구단, Time: 100 ms, Memory: 14100 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,17 +1,29 @@
-import java.io.ByteArrayInputStream;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 
 public class Main {
-    public static void main(String[] args) {
-        int[] multi = {1,2,3,4,5,6,7,8,9};
+    public static void main(String[] args) throws IOException {
+        // 입력을 BufferedReader로 처리
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int stage = Integer.parseInt(reader.readLine()); // 입력값 정수로 변환
 
-		Scanner in = new Scanner(System.in);
-		int stage = in.nextInt();
 
-		for(int i=0; i<=multi.length-1; i++){
-			System.out.println(stage + " * " +  multi[i] +" = "+ stage * multi[i]);
-		}
- 
-		in.close();
+        // StringBuilder를 사용해 출력 효율 개선
+        StringBuilder output = new StringBuilder();
+
+
+        for (int i = 1; i <= 9; i++) { // 1부터 9까지 반복
+            output.append(stage).append(" * ").append(i).append(" = ").append(stage * i).append("\n");
+        }
+
+
+        // 한 번에 출력
+        System.out.print(output);
     }
 }
+
+
+
+
